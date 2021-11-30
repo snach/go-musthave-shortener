badb27ab870fbcd94f3e14f53e1013588e886ccf
Filatova Ksenia <ksu.snach@gmail.com>
2021-11-30 13:13:42 +0300

    sprint_1: all test

--- cmd/shortener/handlers/shortener_test.go
+++ cmd/shortener/handlers/shortener_test.go
@@ -1,11 +1,14 @@
 package handlers
 
 import (
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -89,6 +92,77 @@ func TestGetMethodShortenerHandler(t *testing.T) {
 	}
 }
 
+type errReader int
+
+func (errReader) Read(p []byte) (n int, err error) {
+	return 0, errors.New("test error")
+}
+
 func TestPostMethodShortenerHandler(t *testing.T) {
+	tests := []struct {
+		name       string
+		bodyReader io.Reader
+		statusCode int
+	}{
+		{
+			name:       "positive test",
+			bodyReader: strings.NewReader("https://stackoverflow.com/"),
+			statusCode: 201,
+		},
+		{
+			name:       "negotive test: error in body reader",
+			bodyReader: errReader(0),
+			statusCode: 500,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(http.MethodPost, "/", tt.bodyReader)
+			w := httptest.NewRecorder()
+			h := http.HandlerFunc(ShortenerHandler(map[int]string{1: "https://stepik.org/"}, 2))
+			h.ServeHTTP(w, request)
+			res := w.Result()
+			assert.Equal(t, tt.statusCode, res.StatusCode)
+			if res.StatusCode == 201 {
+				resBody, err := ioutil.ReadAll(res.Body)
+				require.NoError(t, err)
+				err = res.Body.Close()
+				require.NoError(t, err)
+				assert.Equal(t, "http://localhost:8080/2", string(resBody))
+			}
+		})
+	}
+}
+
+func TestUnsupportedMethodShortenerHandler(t *testing.T) {
+	request := httptest.NewRequest(http.MethodHead, "/", nil)
+	w := httptest.NewRecorder()
+	h := http.HandlerFunc(ShortenerHandler(map[int]string{1: "https://stepik.org/"}, 2))
+	h.ServeHTTP(w, request)
+	res := w.Result()
+	assert.Equal(t, 400, res.StatusCode)
+}
+
+func TestIntegrationMapCounterIncrementShortenerHandler(t *testing.T) {
+	h := http.HandlerFunc(ShortenerHandler(make(map[int]string), 1))
+
+	w := httptest.NewRecorder()
+	h.ServeHTTP(w, httptest.NewRequest(http.MethodPost, "/", strings.NewReader("https://stackoverflow.com/")))
+	assert.Equal(t, 201, w.Result().StatusCode)
+
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, httptest.NewRequest(http.MethodPost, "/", strings.NewReader("https://stepik.org/")))
+	assert.Equal(t, 201, w.Result().StatusCode)
+
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, httptest.NewRequest(http.MethodPost, "/", strings.NewReader("https://hh.ru/")))
+	assert.Equal(t, 201, w.Result().StatusCode)
+
+	w = httptest.NewRecorder()
+	h.ServeHTTP(w, httptest.NewRequest(http.MethodGet, "/3", nil))
+	res := w.Result()
+
+	assert.Equal(t, 307, res.StatusCode)
+	assert.Equal(t, "https://hh.ru/", res.Header.Get("Location"))
 
 }

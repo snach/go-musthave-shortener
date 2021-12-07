cfcdf980fb65e08cc2c8e605a56b07e8e94c40d4
Filatova Ksenia <ksu.snach@gmail.com>
2021-12-08 02:03:52 +0300

    sprint_1: fix lint v3

--- cmd/shortener/repository/repository_test.go
+++ cmd/shortener/repository/repository_test.go
@@ -38,8 +38,8 @@ func TestGetRepository(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			repo := Repository{Storage: tt.storage, CurrentInd: 0}
-			fullUrl, err := repo.Get(tt.shortURLID)
-			assert.Equal(t, tt.fullURL, fullUrl)
+			fullURL, err := repo.Get(tt.shortURLID)
+			assert.Equal(t, tt.fullURL, fullURL)
 			if tt.isErr {
 				assert.NotNil(t, err)
 			} else {

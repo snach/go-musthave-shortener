7e95b5217a75e89f824c3f33b7bc891311d7aba4
Filatova Ksenia <ksu.snach@gmail.com>
2021-12-20 17:49:58 +0300

    sprint_2: 6-th increment add tests

--- cmd/shortener/repository/repository_test.go
+++ cmd/shortener/repository/repository_test.go
@@ -2,8 +2,50 @@ package repository
 
 import (
 	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"strings"
 	"testing"
 )
+
+const JSONStringForStorage = "{\"Index\":1,\"FullURL\":\"https://stackoverflow.com/\"}\n" +
+	"{\"Index\":2,\"FullURL\":\"https://stepik.org/\"}\n" +
+	"{\"Index\":3,\"FullURL\":\"https://hh.ru/\"}\n"
+
+func TestNewRepositoryEmptyStorage(t *testing.T) {
+	filename := "TestNewRepositoryFile.txt"
+	repo, err := NewRepository(filename)
+	defer os.Remove(filename)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, repo.CurrentInd)
+	assert.Equal(t, filename, repo.FileName)
+
+	assert.Len(t, repo.Storage, 0)
+}
+
+func TestNewRepositoryNotEmptyStorage(t *testing.T) {
+	filename := "TestNewRepositoryFile.txt"
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+	assert.NoError(t, err)
+	file.Write([]byte(JSONStringForStorage))
+	file.Close()
+	defer os.Remove(filename)
+
+	repo, err := NewRepository(filename)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, repo.CurrentInd)
+	assert.Equal(t, filename, repo.FileName)
+
+	needStorageMap := map[int]string{
+		1: "https://stackoverflow.com/",
+		2: "https://stepik.org/",
+		3: "https://hh.ru/",
+	}
+	assert.Len(t, repo.Storage, 3)
+	assert.Equal(t, needStorageMap[1], repo.Storage[1])
+	assert.Equal(t, needStorageMap[2], repo.Storage[2])
+	assert.Equal(t, needStorageMap[3], repo.Storage[3])
+}
 
 func TestGetRepository(t *testing.T) {
 	tests := []struct {
@@ -48,4 +90,29 @@ func TestGetRepository(t *testing.T) {
 		})
 	}
 
+}
+
+func TestSaveRepository(t *testing.T) {
+	filename := "TestNewRepositoryFile.txt"
+	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+	assert.NoError(t, err)
+	file.Write([]byte(JSONStringForStorage))
+	assert.NoError(t, err)
+	file.Close()
+	defer os.Remove(filename)
+
+	repo, err := NewRepository(filename)
+	assert.NoError(t, err)
+	repo.Save("https://meduza.io/")
+
+	assert.Equal(t, 4, repo.CurrentInd)
+	assert.Equal(t, "https://meduza.io/", repo.Storage[4])
+
+	file, err = os.OpenFile(filename, os.O_CREATE|os.O_RDONLY, 0777)
+	assert.NoError(t, err)
+
+	bytes, err := io.ReadAll(file)
+	assert.NoError(t, err)
+	strs := strings.Split(string(bytes), "\n")
+	assert.Equal(t, "{\"Index\":4,\"FullURL\":\"https://meduza.io/\"}", strs[3])
 }

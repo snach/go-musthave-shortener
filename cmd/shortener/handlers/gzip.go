0f05b374e85eed0961265574351df58b86b3c087
Filatova Ksenia <ksu.snach@gmail.com>
2021-12-22 18:53:51 +0300

    sprint_3: inc 8, gzip

---
+++ cmd/shortener/handlers/gzip.go
@@ -0,0 +1,55 @@
+package handlers
+
+import (
+	"compress/gzip"
+	"io"
+	"net/http"
+	"strings"
+)
+
+type gzipWriter struct {
+	http.ResponseWriter
+	Writer io.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
+//type gzipBodyReader struct {
+//	http.Request
+//	Reader io.ReadCloser
+//}
+//
+//func (r gzipBodyReader) Read(b []byte) (n int, err error){
+//	return r.Reader.Read(b)
+//}
+
+func gzipHandle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		if r.Header.Get("Content-Encoding") == "gzip" {
+			var err error
+			r.Body, err = gzip.NewReader(r.Body)
+			if err != nil {
+				io.WriteString(w, err.Error())
+				return
+			}
+		}
+
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(w, gzip.BestSpeed)
+		if err != nil {
+			io.WriteString(w, err.Error())
+			return
+		}
+		defer gz.Close()
+
+		w.Header().Set("Content-Encoding", "gzip")
+		next.ServeHTTP(gzipWriter{ResponseWriter: w, Writer: gz}, r)
+	})
+}
